72dbc0ff1bf476302d1be8d1b9489632017d0ace
yuli <stremovsky@gmail.com>
2025-04-15 19:33:41 +0300

    use docker-compose if found

--- hard-reset.sh
+++ hard-reset.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-docker-compose down
+# Check if 'docker-compose' command exists
+if command -v docker-compose &> /dev/null; then
+    DC="docker-compose"
+# Fallback to 'docker compose' if the plugin is available
+elif docker compose version &> /dev/null; then
+    DC="docker compose"
+else
+    echo "Neither 'docker-compose' nor 'docker compose' is available."
+    exit 1
+fi
+
+echo "Using: $DC"
+$DC down
 rm -rf data
-docker-compose up -d
+$DC up -d
