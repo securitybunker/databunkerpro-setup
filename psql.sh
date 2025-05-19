c78b6f16542704cefd688e1876b526ed19183c27
EC2 Default User <ec2-user@ip-172-31-32-65.ec2.internal>
2025-05-19 10:03:31 +0000

    fix container name

--- psql.sh
+++ psql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+CONTAINER=`docker ps --filter "name=$(basename $PWD)" --format "{{.Names}}" | grep postgresql`
 source .env/postgresql-postgres.env
-docker exec -ti pro_postgresql_1 /bin/bash -c "PGPASSWORD=$POSTGRES_PASSWORD psql -h 127.0.0.1 -p 5432  -U postgres databunkerdb"
+docker exec -ti $CONTAINER /bin/bash -c "PGPASSWORD=$POSTGRES_PASSWORD psql -h 127.0.0.1 -p 5432  -U postgres databunkerdb"
